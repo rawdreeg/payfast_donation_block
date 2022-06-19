212b546d9f2d77b7509a284f803238e11238864d
Rodrigue Tusse <rodrigue.tusse@automattic.com>
2022-06-19 16:48:52 +0200

    ITN checks.

---
+++ src/Controller/PayfastDonationBlockController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Drupal\payfast_donation_block\Controller;
+
+use Drupal\Component\Serialization\Json;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
+use Drupal\payfast_donation_block\Entity\PayfastDonations;
+use Drupal\payfast_donation_block\PayfastDonationsInterface;
+use Laminas\Diactoros\Response\JsonResponse;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+/**
+ * Returns responses for Payfast Donation Block routes.
+ */
+class PayfastDonationBlockController extends ControllerBase {
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * The logger channel factory.
+   *
+   * @var \Drupal\Core\Logger\LoggerChannelFactoryInterface
+   */
+  protected $logger;
+
+  /**
+   * The config factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * The request stack.
+   *
+   * @var \Symfony\Component\HttpFoundation\RequestStack
+   */
+  protected $requestStack;
+
+  /**
+   * The controller constructor.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   * @param \Drupal\Core\Logger\LoggerChannelFactoryInterface $logger
+   *   The logger channel factory.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The config factory.
+   * @param \Symfony\Component\HttpFoundation\RequestStack $request_stack
+   *   The request stack.
+   */
+  public function __construct(EntityTypeManagerInterface $entity_type_manager, LoggerChannelFactoryInterface $logger, ConfigFactoryInterface $config_factory, RequestStack $request_stack) {
+    $this->entityTypeManager = $entity_type_manager;
+    $this->logger = $logger;
+    $this->configFactory = $config_factory;
+    $this->requestStack = $request_stack;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('entity_type.manager'),
+      $container->get('logger.factory'),
+      $container->get('config.factory'),
+      $container->get('request_stack')
+    );
+  }
+
+  /**
+   * Builds the response.
+   */
+  public function confirmPayment() {
+    $request_data = $this->requestStack->getCurrentRequest()->request->all();
+    $donation = $this->entityTypeManager
+      ->getStorage('payfast_donations')
+      ->load( (int)$request_data['custom_str1']);
+
+    if ($donation instanceof PayfastDonationsInterface) {
+      unset($request_data['signature']);
+      $donation->set('payment_status', $request_data['payment_status']);
+      $donation->set('log', Json::encode($request_data));
+
+      $donation->save();
+    }
+
+   return new JsonResponse(['It works']);
+  }
+
+}
---
+++ src/EventSubscriber/PayfastDonationBlockSubscriber.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace Drupal\payfast_donation_block\EventSubscriber;
+
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Installer\InstallerKernel;
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
+use Drupal\Core\Messenger\MessengerInterface;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\Core\State\State;
+use PayFast\Auth;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+/**
+ * Payfast Donation Block event subscriber.
+ */
+class PayfastDonationBlockSubscriber implements EventSubscriberInterface {
+
+  /**
+   * The logger service.
+   *
+   * @var LoggerChannelFactoryInterface
+   */
+  protected $logger;
+
+  /**
+   * The state service.
+   * @var State
+   */
+  private $state;
+
+  /**
+   * Current route match.
+   * @var RouteMatchInterface
+   */
+  private $route_match;
+
+  /**
+   * Config factory service.
+   * @var ConfigFactoryInterface
+   */
+  private $configFactory;
+
+  /**
+   * Constructs event subscriber.
+   *
+   * @param LoggerChannelFactoryInterface $logger
+   *   The logger service.
+   * @param State $state
+   *   The state service.
+   * @param RouteMatchInterface $route_match
+   *   The route match interface.
+   * @param ConfigFactoryInterface $configFactory
+   *   The config factory.
+   */
+  public function __construct(LoggerChannelFactoryInterface $logger, State $state, RouteMatchInterface $route_match, ConfigFactoryInterface $configFactory) {
+    $this->logger = $logger;
+    $this->state = $state;
+    $this->route_match = $route_match;
+    $this->configFactory = $configFactory;
+  }
+
+  /**
+   * Kernel request event handler.
+   *
+   * @param \Symfony\Component\HttpKernel\Event\RequestEvent $event
+   *   Response event.
+   */
+  public function onKernelRequest(RequestEvent $event) {
+
+    // Make sure front page module is not run when using cli (drush).
+    // Make sure front page module does not run when installing Drupal either.
+    if (PHP_SAPI === 'cli' || InstallerKernel::installationAttempted()) {
+      return;
+    }
+
+    // Don't run when site is in maintenance mode.
+    if ($this->state->get('system.maintenance_mode')) {
+      return;
+    }
+
+    // Ignore non index.php requests (like cron).
+    if (!empty($_SERVER['SCRIPT_FILENAME'])
+      && realpath(DRUPAL_ROOT . '/index.php') != realpath($_SERVER['SCRIPT_FILENAME'])) {
+      return;
+    }
+
+    if ($this->route_match->getRouteName() === 'payfast_donation_block.confirm') {
+      // validate request
+      $request_data = $event->getRequest()->request->all();
+      $payfast_donation_block_config = $this->configFactory
+        ->get('payfast_donation_block.settings');
+
+      if ( empty($request_data)) {
+        throw new AccessDeniedHttpException();
+      }
+      // Validate signature
+      if (! $request_data['signature'] ===  Auth::generateApiSignature($request_data, $payfast_donation_block_config->get('pass_phase')) ) {
+        $this->logger->get('payfast_donation_block')->debug('Could not verify signature', $request_data);
+        throw new AccessDeniedHttpException();
+      }
+
+    }
+
+  }
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    return [
+      KernelEvents::REQUEST => ['onKernelRequest'],
+    ];
+  }
+
+}
